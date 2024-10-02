6f113f7e3001a3bba0a8ebece5e92a319fa15e89
gregorykariara1@gmail.com <gregorykariara1@gmail.com>
2024-10-02 13:13:47 +0300

    Models created

--- project2/app2/models.py
+++ project2/app2/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class BlogPost(models.Model):
+    title = models.TextField(max_length=255)
+    content = models.TextField(max_length=1000)
+
+    def __str__(self):
+        return self.title
